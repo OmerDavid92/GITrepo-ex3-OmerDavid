cb4fb5afdfe63e9a000e38fce72518bce0a00489
Omer David <omerdavid321@gmail.com>
2022-11-16 21:33:54 +0200

    final

--- Omer.java
+++ Omer.java
@@ -1,7 +1,7 @@
 public class Omer { 
 	public static void main(String[] args){
 		for (int i=0; i<=10;i++){
-		System.out.println("Hello Omer new print..."+1);
+		System.out.println("Hello Omer new print another print..."+1);
 		}
 	}
 }
